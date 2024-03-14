d2b5a0347fc93985d532b65e4ef4927d866cdeb5
alvinlai808 <alvinikaika@gmail.com>
2024-03-13 21:28:43 -1000

    added basic login page

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def home():
-    return render_template('home.html')    
+def login():
+    return render_template('login.html')
+
+
+@app.route('/cookie_clicker', methods = ['POST'])
+def cookie_time():
+    return render_template('cookie_clicker.html')
+
 
 app.run()
