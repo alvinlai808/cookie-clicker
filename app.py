768d539015716e6a39eed4e8a8c6c9cad38ac182
alvinlai808 <alvinikaika@gmail.com>
2024-03-14 19:07:17 -1000

    json object adds new users and sets clicks to 0

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
-
+from flask import Flask, render_template, request, redirect, url_for
+import json
+import os.path
 
 app = Flask(__name__)
 
@@ -11,7 +12,27 @@ def login():
 
 @app.route('/cookie_clicker', methods = ['POST'])
 def cookie_clicker():
+    '''
+    - New users will have default count of 0
+    - After signing in, the count should resume from where the user left off
+        - retrieve the user's last known count and display it
+    - Every click sends a POST request to increment the particular user's count
+    6:35pm
+    '''
+    users = {}
+
+    if os.path.exists('users.json'):
+        with open('users.json') as users_file:
+            users = json.load(users_file)
+
+    if request.form['username'] not in users.keys():
+        users[request.form['username']] = 0
+
+    with open('users.json', 'w') as user_file:
+        json.dump(users, user_file)
+
     return render_template('cookie_clicker.html', username=request.form['username'])
+
 
 
 app.run(debug = True, host="localhost", port="8080")
