beacfbc74c4975e09490e6974d9ad7a0cf4d4f23
alvinlai808 <alvinikaika@gmail.com>
2024-03-30 17:51:46 -1000

    game resumes from user's last click amount

--- app.py
+++ app.py
@@ -13,11 +13,9 @@ def login():
 @app.route('/cookie_clicker', methods = ['POST'])
 def cookie_clicker():
     '''
-    - New users will have default count of 0
     - After signing in, the count should resume from where the user left off
         - retrieve the user's last known count and display it
     - Every click sends a POST request to increment the particular user's count
-    6:35pm
     '''
     users = {}
 
@@ -33,6 +31,16 @@ def cookie_clicker():
 
     return render_template('cookie_clicker.html', username=request.form['username'])
 
+@app.route('/update-click', methods = ['POST'])
+def update_click():
+    if os.path.exists('users.json'):
+        with open('users.json') as users_file:
+            users = json.load(users_file)
 
+    users[request.form['username']] += 1
+
+    with open('users.json', 'w') as user_file:
+        json.dump(users, user_file)
+    return render_template('cookie_clicker.html', username=request.form['username'], clicks=users[request.form['username']])
 
 app.run(debug = True, host="localhost", port="8080")
